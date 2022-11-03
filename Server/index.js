34c6097217d77e3e581f5e22471765a3aeb05ce2
Aditya Bhate <adityabhate305@gmail.com>
2022-11-03 19:33:26 +0530

    added error handling and CORS

--- Server/index.js
+++ Server/index.js
@@ -1,26 +1,64 @@
 const express = require("express");
 const config = require("./src/config/config");
 const bodyParser = require("body-parser");
-const  dbConnect= require("./src/service/db");
+const dbConnect = require("./src/service/db");
+const errorMiddleware = require("./src/middleware/error");
 
+const connectApp = async () => {
+	const app = express();
+	//adding CORS
+	const allowedOrigins = ["http://localhost:3000"];
+	app.use(
+		cors({
+			origin: function (origin, callback) {
+				// allow requests with no origin (like mobile apps or curl requests)
+				if (!origin) return callback(null, true);
+				// allow whitelisted origins
+				if (allowedOrigins.indexOf(origin) === -1)
+					return callback(
+						new Error(
+							"The CORS policy for this site does not allow access from the specified Origin."
+						),
+						false
+					);
+				// else
+				return callback(null, true);
+			},
+			credentials: true,
+		})
+	);
+	//middleware
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(express.json());
+	app.use(errorMiddleware);
+	app.use(express.json());
+	//database connection
+	try {
+		await dbConnect.dbConnect();
+		console.log("Database Connected");
+	} catch (error) {
+		console.log(error);
+	}
+	//? Handling Uncaught Exceptions
+	process.on("uncaughtException", (err) => {
+		console.log(`Error: ${err.message}`);
+		console.log(`Shutting down the server due to Uncaught Exception`);
 
-const connectApp=async ()=>{
-    try {
-        await dbConnect.dbConnect();
-        console.log('Database Connected');
-    } catch (error) {
-        console.log(error);
-    }
-    
-    const app = express();
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
-    app.use(express.json());
+		process.exit(1);
+	});
+	//server connection
+	app.listen(config.PORT, () => {
+		console.log(`Server running on port ${config.PORT}`);
+	});
+	//? Unhandled Promise Rejection
+	process.on("unhandledRejection", (err) => {
+		console.log(`Error: ${err.message}`);
+		console.log(`Shutting down the server due to Unhandled Promise Rejection`);
 
-    app.listen(config.PORT, () => {
-    console.log(`Server running on port ${config.PORT}`);
-    });
-
-}
+		server.close(() => {
+			process.exit(1);
+		});
+	});
+};
 
 connectApp();
