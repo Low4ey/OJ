c0cecc22660257ddb0ff5433293575c9e1b26426
Ajeet Kumar <ajeet8364@gmail.com>
2023-05-20 23:02:52 +0530

    1.2.8
    Added Major Routes

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,9 +1,14 @@
 const userRouter=require("./userRoutes");
 const tokenRouter=require("./tokenRoutes");
 const problemRouter=require("./problemRoutes");
+const testCaseRouter=require("./testCaseRoutes");
+const editorialRouter=require("./editorialRoutes");
 
 module.exports={
     userRouter,
     tokenRouter,
     problemRouter,
+    testCaseRouter,
+    editorialRouter,
+
 }
