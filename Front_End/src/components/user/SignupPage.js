d5522f4f0f27c04af8d05ddab761e38ee9c94382
Jal Thakor <jalsthakor413@gmail.com>
2023-05-27 09:56:18 +0530

    added custom colors in tailwind

--- Front_End/src/components/user/SignupPage.js
+++ Front_End/src/components/user/SignupPage.js
@@ -102,70 +102,71 @@ const SignupPage = () => {
 		}
 	};
 
-	return (
-		<div className="flex justify-center items-center min-h-screen bg-gray-900">
-			<div className="bg-gray-800 rounded-lg p-8 max-w-md w-full">
-				<h2 className="text-3xl text-white mb-6">Signup</h2>
-				{error && <p className="text-red-500 mb-2">{error}</p>}
-				<form onSubmit={handleSubmit}>
-					<input
-						className="w-full mb-4 px-4 py-2 rounded-lg bg-gray-700 text-white placeholder-gray-400"
-						type="text"
-						name="userName"
-						placeholder="Username"
-						value={formData.userName}
-						onChange={handleChange}
-					/>
-					<input
-						className="w-full mb-4 px-4 py-2 rounded-lg bg-gray-700 text-white placeholder-gray-400"
-						type="text"
-						name="firstName"
-						placeholder="First Name"
-						value={formData.firstName}
-						onChange={handleChange}
-					/>
-					<input
-						className="w-full mb-4 px-4 py-2 rounded-lg bg-gray-700 text-white placeholder-gray-400"
-						type="text"
-						name="lastName"
-						placeholder="Last Name"
-						value={formData.lastName}
-						onChange={handleChange}
-					/>
-					<input
-						className="w-full mb-4 px-4 py-2 rounded-lg bg-gray-700 text-white placeholder-gray-400"
-						type="email"
-						name="userEmail"
-						placeholder="Email"
-						value={formData.userEmail}
-						onChange={handleChange}
-					/>
-					<input
-						className="w-full mb-4 px-4 py-2 rounded-lg bg-gray-700 text-white placeholder-gray-400"
-						type="tel"
-						name="userPhone"
-						placeholder="Phone"
-						value={formData.userPhone}
-						onChange={handleChange}
-					/>
-					<input
-						className="w-full mb-4 px-4 py-2 rounded-lg bg-gray-700 text-white placeholder-gray-400"
-						type="password"
-						name="userPassword"
-						placeholder="Password"
-						value={formData.userPassword}
-						onChange={handleChange}
-					/>
-					<button
-						className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-						type="submit"
-					>
-						Sign Up
-					</button>
-				</form>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-900">
+      <div className="bg-gray-800 rounded-lg p-8 max-w-md w-full">
+        <h2 className="text-3xl text-white mb-6">Signup</h2>
+        {error && <p className="text-red-500 mb-2">{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <input
+            className="w-full mb-4 px-4 py-2 rounded-lg bg-custom-gray text-white placeholder-gray-400"
+            type="text"
+            name="userName"
+            placeholder="Username"
+            value={formData.userName}
+            onChange={handleChange}
+          />
+          <input
+            className="w-full mb-4 px-4 py-2 rounded-lg bg-custom-gray text-white placeholder-gray-400"
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+          <input
+            className="w-full mb-4 px-4 py-2 rounded-lg bg-custom-gray text-white placeholder-gray-400"
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+          <input
+            className="w-full mb-4 px-4 py-2 rounded-lg bg-custom-gray text-white placeholder-gray-400"
+            type="email"
+            name="userEmail"
+            placeholder="Email"
+            value={formData.userEmail}
+            onChange={handleChange}
+          />
+          <input
+            className="w-full mb-4 px-4 py-2 rounded-lg bg-custom-gray text-white placeholder-gray-400"
+            type="tel"
+            name="userPhone"
+            placeholder="Phone"
+            value={formData.userPhone}
+            onChange={handleChange}
+          />
+          <input
+            className="w-full mb-4 px-4 py-2 rounded-lg bg-custom-gray text-white placeholder-gray-400"
+            type="password"
+            name="userPassword"
+            placeholder="Password"
+            value={formData.userPassword}
+            onChange={handleChange}
+          />
+          <button
+            className="w-full bg-custom-blue hover:bg-custom-dark-blue text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+  
 };
 
 export default SignupPage;
--- Front_End/tailwind.config.js
+++ Front_End/tailwind.config.js
@@ -1,9 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-gray': {
+          DEFAULT: '#4C3A51',
+        },
+        'custom-blue': {
+          DEFAULT: '#774360',
+        },
+        'custom-dark-blue': {
+          DEFAULT: '#B25068',
+        },
+        'custom-orange': {
+          DEFAULT: '#E7AB79',
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
