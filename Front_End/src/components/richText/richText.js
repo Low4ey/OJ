5c061351f8f8b7fbc3be90bae562c99244c8bb4e
Harsh Deepanshu <harshdeepanshustrix@gmail.com>
2023-06-03 16:40:38 +0530

    Fixed initial render issue of text editor on problem.js file under Add Problem.

--- Front_End/src/components/richText/richText.js
+++ Front_End/src/components/richText/richText.js
@@ -1,43 +1,37 @@
-import React, { useRef, useEffect } from 'react';
-import Quill from 'quill';
-import 'quill/dist/quill.snow.css';
-import "./RichTextEditor.css"
+import React, { useRef, useEffect } from "react";
+// import Quill from "quill";
+import ReactQuill from 'react-quill';
+import "quill/dist/quill.snow.css";
+import "./RichTextEditor.css";
 const MarkdownEditor = ({ content, onContentChange }) => {
   const quillRef = useRef(null);
 
   useEffect(() => {
-    quillRef.current = new Quill('#editor', {
-      theme: 'snow',
-      modules: {
-        toolbar: [
-          ['bold', 'italic', 'underline', 'strike'], // Formatting options
-          [{ 'list': 'ordered' }, { 'list': 'bullet' }], // List options
-          ['link', 'image'], // Link and image insertion
-        ],
-      },
-    });
-
-    // Update markdown text when content changes
-    quillRef.current.on('text-change', () => {
-      const markdownText = quillRef.current.root.innerHTML;
-      
-      onContentChange(markdownText);
-      // Use the markdown text as needed
-    });
-  }, [onContentChange]);
-
-  const handleInsertImage = () => {
-    const imageUrl = prompt('Enter the URL of the image:');
-    if (imageUrl) {
-      const range = quillRef.current.getSelection();
-      quillRef.current.insertEmbed(range.index, 'image', imageUrl);
+    if (!quillRef.current) {
+      quillRef.current = true;
     }
+  }, []);
+
+  const handleTextChange = (value) => {
+    onContentChange(value);
+  };
+
+  const modules = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'], // Formatting options
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }], // List options
+      ['link', 'image'], // Link and image insertion
+    ],
   };
 
   return (
     <div className="markdown-editor">
-      <div id="toolbar" />
-      <div id="editor" />
+      <ReactQuill
+        theme="snow"
+        value={content}
+        onChange={handleTextChange}
+        modules={modules}
+      />
     </div>
   );
 };
--- Server/src/config/config.js
+++ Server/src/config/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 module.exports = Object.freeze({
-	PORT: process.env.PORT,
-	DB: process.env.DB_URI,
-	ACCESS_TOKEN_PRIVATE_KEY: process.env.ATPK,
-	REFRESH_TOKEN_PRIVATE_KEY: process.env.RTPK,
-	JWT_SECRET:process.env.JWT_SECRET,
-	SALT: process.env.SALT
-})
+  PORT: process.env.PORT || 8007, // just added it so that even if env congfig is not present i will run smoothly on other pc.
+  DB: process.env.DB_URI,
+  ACCESS_TOKEN_PRIVATE_KEY: process.env.ATPK,
+  REFRESH_TOKEN_PRIVATE_KEY: process.env.RTPK,
+  JWT_SECRET: process.env.JWT_SECRET,
+  SALT: process.env.SALT,
+});
