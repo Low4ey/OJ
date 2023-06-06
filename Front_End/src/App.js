8b3b3b789b20ffc3e7eec428283454df98f0a64f
Harsh Deepanshu <harshdeepanshustrix@gmail.com>
2023-06-06 11:28:07 +0530

    fixed multi component render issue

--- Front_End/src/App.js
+++ Front_End/src/App.js
@@ -3,33 +3,25 @@ import RichTextEditor from "./components/richText/richText";
 import CodeEditor from "./components/codeEditor/codeEditor";
 import ProblemPage from "./pages/ShowProblem/showProblem";
 import AddProblem from "./pages/AddProblem/problem"
-
 import ProblemList from "./pages/ShowProblem/listProblem";
-
-import NavigationSteps from './components/Navbar/steps';
-
 
 // import LoginPage from "./components/user/LoginPage";
 // import SignupPage from "./components/user/SignupPage";
 
 const App = () => {
-    const steps = ['Step 1', 'Step 2', 'Step 3', 'Step 4'];
-
     return (
         <div>
-
             Welcome to my MERN stack application [HOME]
-
-            <NavigationSteps steps={steps} />
             {/* <LoginPage /> */}
             {/* <SignupPage /> */}
             {/* <RichTextEditor /> */}
             {/* <CodeEditor /> */}
-
             {/* <ProblemPage /> */}
             {/* <AddProblem /> */}
             <ProblemList />
-            {/* <AddProblem /> */}
+
+
+            
         </div>
     );
 };
