28bec15dedba83e60e343fe477f51d48b943ffa9
Harsh Deepanshu <harshdeepanshustrix@gmail.com>
2023-06-07 00:26:23 +0530

    fixed console errors

--- Front_End/src/index.js
+++ Front_End/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Fragment } from 'react';
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
@@ -13,23 +13,20 @@ import DashboardPage from "./pages/dashboard";
 
 import './tailwind.css'; // Import Tailwind CSS styles
 const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
 
-
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
+root.render(
+  <Fragment>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/add-problem" element={<EditorPage/>} />
+        <Route path="/add-problem" element={<EditorPage />} />
         {/* <Route path="/problem/" element={<ListProblemPage />} /> */}
         <Route path="/dashboard/:problemTitle" element={<ProblemDashboard />} />
-        <Route path="/dashboard" element={<DashboardPage/>} />
-
-
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Fragment>
 );
