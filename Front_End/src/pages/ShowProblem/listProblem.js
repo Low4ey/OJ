910b146e97eeccc89ef96f1c348179d32e6d1452
Jal Thakor <jalsthakor413@gmail.com>
2023-06-08 18:06:36 +0530

    added authuser on dashboard

--- Front_End/src/pages/ShowProblem/listProblem.js
+++ Front_End/src/pages/ShowProblem/listProblem.js
@@ -1,59 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import DOMPurify from "dompurify";
+
+
+const getAccessToken = () => {
+	const encodedToken = localStorage.getItem("accessToken");
+	if (encodedToken) {
+		const sanitizedToken = decodeURIComponent(encodedToken);
+		return DOMPurify.sanitize(sanitizedToken);
+	}
+	return null;
+};
 
 const ProblemList = () => {
-  const [problems, setProblems] = useState([]);
-  const navigate = useNavigate();
+	const [problems, setProblems] = useState([]);
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    fetchProblems();
-  }, []);
+	useEffect(() => {
+		fetchProblems();
+	}, []);
 
-  const fetchProblems = () => {
-    fetch('http://localhost:5005/api/getProblem')
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } 
-        else if(response.status === 401){
-          navigate(`/login`)
-          // navigate(`/dashboard/DashboardPage`) // For Testing
-        }
-        else {
-          throw new Error('Error: ' + response.status);
-        }
-      })
-      .then(data => setProblems(data))
-      .catch(error => {
-        console.error('Error fetching problems:', error);
-      });
-  };
+	const accessToken = getAccessToken();
 
-  const convertProblemName = (title) => {
-    return title.toLowerCase().replace(/\s+/g, '-');
-  };
-  
+	const config = {
+		headers: {
+			Authorization: `Bearer ${accessToken}`,
+			"Content-Type": "application/json",
+			"X-Requested-With": "XMLHttpRequest",
+		},
+		withCredentials: true,
+	};
 
-  const redirectToProblemPage = (problemTitle) => {
-    navigate(`/dashboard/${problemTitle}`);
-  };
+	const fetchProblems = () => {
+		fetch("http://localhost:5005/api/getProblem", {
+			method: "GET",
+			headers: config.headers,
+			credentials: "include",
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				} else if (response.status === 401) {
+					navigate(`/login`);
+					// navigate(`/dashboard/DashboardPage`) // For Testing
+				} else {
+					throw new Error("Error: " + response.status);
+				}
+			})
+			.then((data) => setProblems(data))
+			.catch((error) => {
+				console.error("Error fetching problems:", error);
+			});
+	};
 
-  return (
-    <div className="flex">
-      <div className="w-1/3 bg-gray-200 p-4">
-        <h1 className="text-2xl font-bold mb-4">Problems</h1>
-        {problems.map((problem) => (
-          <div
-            key={problem._id}
-            className="cursor-pointer mb-2 p-2 border border-gray-300"
-            onClick={() => redirectToProblemPage(convertProblemName(problem.title))}
-          >
-            {problem.title}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	const convertProblemName = (title) => {
+		return title.toLowerCase().replace(/\s+/g, "-");
+	};
+
+	const redirectToProblemPage = (problemTitle) => {
+		navigate(`/dashboard/${problemTitle}`);
+	};
+
+	return (
+		<div className="flex">
+			<div className="w-1/3 bg-gray-200 p-4">
+				<h1 className="text-2xl font-bold mb-4">Problems</h1>
+				{problems.map((problem) => (
+					<div
+						key={problem._id}
+						className="cursor-pointer mb-2 p-2 border border-gray-300"
+						onClick={() =>
+							redirectToProblemPage(
+								convertProblemName(problem.title)
+							)
+						}
+					>
+						{problem.title}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default ProblemList;
