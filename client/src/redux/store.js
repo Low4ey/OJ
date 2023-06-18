03903e45c6812ec5a1e690d3d13fa343701768dd
Jal Thakor <jalsthakor413@gmail.com>
2023-06-18 10:23:15 +0530

    redux init

---
+++ client/src/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers/loginReducers';
+
+const store = configureStore({ reducer: rootReducer });
+
+export default store;
+
